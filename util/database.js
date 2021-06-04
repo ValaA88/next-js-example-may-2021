6415a0c4fabd9e8f4259c3f99219579e46ac19db
Karl Horky <karl.horky@gmail.com>
2021-06-04 12:12:23 +0200

    Avoid connection errors with database

--- util/database.js
+++ util/database.js
@@ -6,8 +6,28 @@ import postgres from 'postgres';
 // (host, database, username, password) from the .env file
 dotenvSafe.config();
 
-// Connect to the database
-const sql = postgres();
+// Connect only once to the database
+// https://github.com/vercel/next.js/issues/7811#issuecomment-715259370
+function connectOneTimeToDatabase() {
+  let sql;
+
+  if (process.env.NODE_ENV === 'production') {
+    // Heroku needs SSL connections but
+    // has an "unauthorized" certificate
+    // https://devcenter.heroku.com/changelog-items/852
+    sql = postgres({ ssl: { rejectUnauthorized: false } });
+  } else {
+    if (!globalThis.__postgresSqlClient) {
+      globalThis.__postgresSqlClient = postgres();
+    }
+    sql = globalThis.__postgresSqlClient;
+  }
+
+  return sql;
+}
+
+// Connect to PostgreSQL
+const sql = connectOneTimeToDatabase();
 
 // Perform a first query
 export async function getUsers() {
