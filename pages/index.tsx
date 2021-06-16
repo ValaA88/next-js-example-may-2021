37d21ffea9fe7889b3e4e6f0f0b16be11e7f2f04
Karl Horky <karl.horky@gmail.com>
2021-06-16 10:21:20 +0200

    Upgrade to Next.js 11

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import { css } from '@emotion/react';
 import Head from 'next/head';
+// import Image from 'next/image';
 import Layout from '../components/Layout';
+
+// import karl from './karl.jpg';
 
 const imgStyles = css`
   max-width: 100%;
@@ -17,15 +20,10 @@ export default function Home() {
 
         {/*
           This is currently crashing on my M1 MacBook:
-
-          <h2>Using the `Image` component from Next.js</h2>
-          <Image
-            src="/karl.jpg"
-            alt="llamas mit huten"
-            width="1280"
-            height="720"
-          />
         */}
+
+        <h2>Using the `Image` component from Next.js</h2>
+        {/* <Image src={karl} alt="llamas mit huten" placeholder="blur" /> */}
 
         <h2>Using the HTML `img` tag</h2>
         <img css={imgStyles} src="/karl.jpg" alt="llamas mit huten" />
